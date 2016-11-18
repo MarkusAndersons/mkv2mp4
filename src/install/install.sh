b80f0a3d173b1c90fde47bf7d77d20453a4e1ac3
MarkusAndersons <markus.andersons@gmail.com>
2016-11-18 15:37:39 +1030

    Windows installation scripts

---
+++ src/install/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# check if root
+if [ "$EUID" -ne 0 ]
+    then echo "Must be root"
+    echo "Usage: sudo make install"
+    exit 1
+fi
+
+# check for python
+command -v python >/dev/null 2>&1 || {
+    echo "Installing ..."
+    echo "---------------------"
+    echo "Downloading ..."
+    curl -o ffmpeg.tar.bz2  'https://cdn.markusandersons.com/static/ffmpeg.tar.bz2'
+    tar -xzvf ffmpeg.tar.bz2
+    mv ffmpeg /usr/local/bin
+    rm ffmpeg.tar.bz2
+}
+
+#   check if already installed
+command -v ffmpeg >/dev/null 2>&1 || {
+    echo "Installing ..."
+    echo "---------------------"
+    echo "Downloading ..."
+    curl -o ffmpeg.tar.bz2  'https://cdn.markusandersons.com/static/ffmpeg.tar.bz2'
+    tar -xzvf ffmpeg.tar.bz2
+    mv ffmpeg /usr/local/bin
+    rm ffmpeg.tar.bz2
+}
+
+chmod +x convert_mkv
+echo "Installed"
