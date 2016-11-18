b871e7b0c56927b4d7f7a8e1f6e6051e27a7164e
MarkusAndersons <markus.andersons@gmail.com>
2016-11-18 14:10:04 +1030

    Basic Unix code complete

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # mkv2mp4
-A simple GUI for converting .mkv files to .mp4 using the ffmpeg library. The program is written entirely in Python and is a cross-platform compatible (tested on macOS Sierra and Windows 10).
+A simple GUI for converting .mkv files to .mp4 using the ffmpeg library. The program is written entirely in Python and is compatible cross-platform (tested on macOS Sierra and Windows 10).
 
 ## Installation
+
+## Usage
+To use the application on Windows, ...
--- src/mkv2mp4.py
+++ src/mkv2mp4.py
@@ -12,8 +12,31 @@ root.withdraw()
 # env variables
 isWindows = 0
 isPosix = 0
+ffmpegLocation = None
+
+# to find the ffmpeg exe
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 def checkDetails():
+    global isWindows
+    global isPosix
+    global ffmpegLocation
     # check OS
     if os.name == 'nt':
         isWindows = 1
@@ -25,12 +48,24 @@ def checkDetails():
         messagebox.showinfo("ERROR", "Sorry, " + os.name + " is unsupported at this time")
         sys.exit()
     # check if FFmpeg installed
-    
+    ffmpegLocation = which("ffmpeg")
+    if ffmpegLocation is None:
+        print("ERROR: ffmpeg not installed")
+        messagebox.showinfo("ERROR", "The ffmpeg executable cannot be found, please make sure you have correctly installed this program and all dependencies")
+        sys.exit()
 
-def convert():
-    x = 1
+def convert(filename):
+    #ffmpeg -i $fullfilename -c copy "${fullfilename%.mkv}.mp4"
+    outName = filename[:-4] + ".mp4"
+    print outName
+    print filename
+    if isPosix:
+        call(ffmpegLocation + " -i \"" + filename + "\" -c copy \"" + outName + "\"", shell = True)
 
 checkDetails()
 
 # file selector
 file_path = filedialog.askopenfilename()
+root.update()
+if file_path is not None:
+    convert(file_path)
