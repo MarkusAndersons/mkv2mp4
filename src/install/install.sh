ad7b817e1d6b589b87975c28ceb76016a618401d
MarkusAndersons <markus.andersons@gmail.com>
2016-11-21 14:02:11 +1030

    Added uninstall option

--- src/install/install.sh
+++ src/install/install.sh
@@ -6,6 +6,12 @@ if [ "$EUID" -ne 0 ]
     exit 1
 fi
 
+if [ "$1" == "uninstall" ] ; then
+    rm -rf /usr/local/mkv2mp4_files
+    rm /usr/local/bin/mkv2mp4
+    echo "Uninstalled"
+    exit 0
+fi
 # check for python
 command -v python >/dev/null 2>&1 || {
     echo "ERROR - Python must be installed"
@@ -21,7 +27,12 @@ command -v ffmpeg >/dev/null 2>&1 || {
     tar -xzvf ffmpeg.tar.bz2
     mv ffmpeg /usr/local/bin
     rm ffmpeg.tar.bz2
+    echo "Installed ffmpeg"
 }
 
-chmod +x convert_mkv
-echo "Installed"
+mkdir /usr/local/mkv2mp4_files
+cp ../mkv2mp4.py /usr/local/mkv2mp4_files
+touch /usr/local/bin/mkv2mp4
+echo "python ../mkv2mp4_files/mkv2mp4.py" >> /usr/local/bin/mkv2mp4
+chmod +x /usr/local/bin/mkv2mp4
+echo "Installed mkv2mp4"
