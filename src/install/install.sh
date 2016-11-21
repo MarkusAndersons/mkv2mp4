bfcbaaa6f89e70f906529163c5d8d760fa0d43c1
MarkusAndersons <markus.andersons@gmail.com>
2016-11-21 13:53:00 +1030

    Basic POSIX install script done

--- src/install/install.sh
+++ src/install/install.sh
@@ -2,19 +2,14 @@
 # check if root
 if [ "$EUID" -ne 0 ]
     then echo "Must be root"
-    echo "Usage: sudo make install"
+    echo "Usage: sudo bash install.sh"
     exit 1
 fi
 
 # check for python
 command -v python >/dev/null 2>&1 || {
-    echo "Installing ..."
-    echo "---------------------"
-    echo "Downloading ..."
-    curl -o ffmpeg.tar.bz2  'https://cdn.markusandersons.com/static/ffmpeg.tar.bz2'
-    tar -xzvf ffmpeg.tar.bz2
-    mv ffmpeg /usr/local/bin
-    rm ffmpeg.tar.bz2
+    echo "ERROR - Python must be installed"
+    exit 2
 }
 
 #   check if already installed
